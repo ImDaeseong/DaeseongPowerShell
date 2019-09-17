bc4b011b76e84bb7cdb9bb88c5bee4d837819c4a
ImDaeseong <cs93059@gmail.com>
2019-09-17 10:33:44 +0900

    DaeseongPowerShell

---
+++ ipconfig/ipconfig/Script.ps1
@@ -0,0 +1,32 @@
+﻿
+function GetEstablishedProcess {
+	Get-Process -Id (Get-NetTCPConnection -State Established | Sort-Object LocalPort).OwningProcess
+}
+
+
+#네트워크 인터페이스 정보 확인
+#Get-NetIPInterface
+
+
+#네트워크 인터페이스 세부 정보 확인
+#Get-NetIPConfiguration
+
+
+#TCP 연결 상태 조회
+#Get-NetTCPConnection -State Established | Sort-Object LocalPort
+
+
+#NSLookup
+#Resolve-DnsName naver.com
+
+
+#Ping 
+#Test-NetConnection naver.com -TraceRoute
+
+
+GetEstablishedProcess
+
+
+
+
+
