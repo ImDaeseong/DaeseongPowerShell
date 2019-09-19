bf4e2b32202cb04b95903d446e32625c7f62ba1d
ImDaeseong <cs93059@gmail.com>
2019-09-19 12:12:39 +0900

    DaeseongPowerShell

---
+++ Start-Process/Start-Process/Script.ps1
@@ -0,0 +1,15 @@
+﻿
+# 프로세스 실행후 계속 유지
+#Start-Process cmd.exe -Wait 
+
+# 프로세스 실행후 종료
+#Start-Process cmd.exe
+
+
+#메모장 실행
+#Start-Process notepad.exe
+
+
+#ie 실행
+$sPath = "C:\Program Files (x86)\Internet Explorer\IEXPLORE.EXE" 
+Start-Process $sPath 
