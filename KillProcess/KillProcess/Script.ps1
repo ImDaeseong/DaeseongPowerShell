3fa2709cb3b218981668ce7774e389c18dd44e40
ImDaeseong <cs93059@gmail.com>
2019-10-10 09:58:44 +0900

    DaeseongPowerShell

---
+++ KillProcess/KillProcess/Script.ps1
@@ -0,0 +1,12 @@
+﻿
+Function Kill-Process($programName)
+{	
+	Clear-Host
+	$process = Get-Process $programName
+	$process.Kill()
+}
+
+Kill-Process "SearchIndexer"
+Kill-Process "SkypeApp"
+Kill-Process "SkypeBackgroundHost"
+Kill-Process "RtkAudUService64"
