af8309af28a01e8adeb282f302b09c49012f8d97
ImDaeseong <cs93059@gmail.com>
2019-10-11 09:22:07 +0900

    DaeseongPowerShell

---
+++ RegistryInfo/RegistryInfo/Script.ps1
@@ -0,0 +1,9 @@
+﻿
+# 레지스트리 정보 읽기 
+$curReg = Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Internet Explorer\Main'
+Write-Output $curReg
+
+
+# 레지스트리 정보 읽기
+$macReg = Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\Main'
+Write-Output $macReg
