1b9d5bb5fe27b0b25316d4e8e784dfc1566bb25c
ImDaeseong <cs93059@gmail.com>
2019-10-29 11:13:31 +0900

    DaeseongPowerShell

---
+++ ServiceInfo/ServiceInfo/Script.ps1
@@ -0,0 +1,41 @@
+﻿
+<# 
+#서비스명을 직접 입력받아 서비스 정보 확인
+$servicename=read-host "확인할 서비스명을 입력하세요"
+Get-Service -Name $servicename
+#>
+
+
+#실행중 서비스
+#Get-Service | where {$_.Status -eq "Running"}
+
+
+#b로 시작하는 서비스명
+#Get-Service | where {$_.Name -like "b*"}
+
+<#
+$servicename = Get-Service -Name "서비스명"
+Write-Host $servicename 
+#>
+
+#서비스 중지
+#Stop-Service "서비스명"
+
+#서비스 제거
+#Remove-Service -Name "서비스명"
+
+#서비스 시작
+#Start-Service "서비스명"
+
+#서비스 생성
+<#
+$filepath = "파일 경로"
+New-Service -Name "서비스명" -DisplayName "서비스 설명" -StartupType Automatic -BinaryPathName $filepath
+#>
+
+
+#cmd 서비스 제거시 사용
+#sc delete "서비스명"
+
+
+Get-Service
---
+++ netstat-ano/netstat-ano/Script.ps1
@@ -0,0 +1,19 @@
+﻿
+Function Get-Netstat-findstr
+{	
+	netstat -ano | findstr ESTABLISHED      #연결 상태
+	#netstat -ano | findstr LISTENING       #포트 열림, 연결 기다리는 상태
+	#netstat -ano | findstr TIME_WAIT       #연결 종료, 다음 연결 대기 상태
+	#netstat -ano | findstr CLOSED_WAIT     #연결 종료되길 기다리는 상태
+}
+
+Function Get-Netstat-ano
+{	
+	netstat -ano
+}
+
+# 포트 연결 상태 
+#Get-Netstat-findstr
+
+# 포트 전체 정보 
+Get-Netstat-ano
