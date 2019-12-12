428c6ce09f4de9f757370fb899e96e065847e6a1
ImDaeseong <cs93059@gmail.com>
2019-12-12 10:09:12 +0900

    DaeseongPowerShell

---
+++ goinstall/goinstall/Script.ps1
@@ -0,0 +1,8 @@
+﻿Write-Output "github.com/gorilla/websocket 설치"
+go get -u github.com/gorilla/websocket
+
+Write-Output "설치 목록"
+go list ...
+
+Write-Output "1분 대기후 종료"
+Start-Sleep -s 60
---
+++ pipinstall/pipinstall/Script.ps1
@@ -0,0 +1,8 @@
+﻿Write-Output "pyshark 설치"
+pip install pyshark
+
+Write-Output "설치 목록"
+pip list
+
+Write-Output "1분 대기후 종료"
+Start-Sleep -s 60
