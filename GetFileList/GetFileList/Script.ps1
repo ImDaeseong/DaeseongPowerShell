c2bd4ec39440e06aa0c726698ae6cbb632968732
ImDaeseong <cs93059@gmail.com>
2020-12-03 18:07:25 +0900

    DaeseongPowerShell

---
+++ GetFileList/GetFileList/Script.ps1
@@ -0,0 +1,14 @@
+﻿# 전체 파일
+
+$fileList = Get-ChildItem "E:\DaeseongSwift" *.* -Recurse | Select-Object FullName
+
+foreach ($file in $fileList) {
+
+    $fileFullPath = $file.fullname
+   
+    Write-Output $fileFullPath
+}
+
+Write-Output "1분 대기후 종료"
+Start-Sleep -s 60
+
